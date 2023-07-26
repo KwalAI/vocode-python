656d5f8e2dfa95da38ecd114de111acea02843aa
anunayajoshi <47421902+anunayajoshi@users.noreply.github.com>
2023-07-26 12:09:27 +0800

    plotting stft graph

--- vocode/streaming/utils/noise_detection.py
+++ vocode/streaming/utils/noise_detection.py
@@ -3,6 +3,7 @@ import numpy as np
 import time
 import librosa
 import io
+import matplotlib.pyplot as plt
 
 
 class NoiseDetector:
@@ -15,6 +16,8 @@ class NoiseDetector:
         self.detected_noise = False
         self.interrupted = False
         self.last_print_time = time.time()
+        self.created = False
+        self.count = 40
 
     def receive_audio(self, chunk: bytes):
         audio_data = np.frombuffer(chunk, dtype=np.int16)
@@ -22,12 +25,26 @@ class NoiseDetector:
         n_fft = min(len(audio_data), 2048)
         stft_matrix = librosa.stft(audio_data, n_fft=n_fft)
         magnitude_spectogram = np.abs(stft_matrix)
+        if time.time() - self.last_print_time > 5:
+            self.count += 1
+            self.last_print_time = time.time()
+            fig, ax = plt.subplots()
+            img = librosa.display.specshow(
+                librosa.amplitude_to_db(magnitude_spectogram, ref=np.max),
+                ax=ax,
+                y_axis="log",
+                x_axis="time",
+            )
+            ax.set_title("Power spectrogram")
+            fig.colorbar(img, ax=ax, format="%+2.0f dB")
+            plt.savefig(f"spectogram{self.count}.png")
         magnitude_spectogram = np.mean(
             magnitude_spectogram, axis=1
         )  # merge across time axis into a 1 dimensional array containing magnitude of each frequency bin
+
         threshold = np.mean(magnitude_spectogram) + np.std(magnitude_spectogram)
         num_bins_above_threshold = np.sum(magnitude_spectogram > threshold)
-        print("num bins above threshold", num_bins_above_threshold)
+        # print("num bins above threshold", num_bins_above_threshold)
 
         # # audio_segment = AudioSegment(
         # #     data=chunk, sample_width=2, frame_rate=16000, channels=1
